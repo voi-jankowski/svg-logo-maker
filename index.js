bca1fa0a64664e9a5d1fc9a4f1d2effeefbc2258
Voi <wojtek.wacek.dola@gmail.com>
2023-04-21 10:24:00 +1000

    created async function run and passed the inquirerResult to createSVG function.

--- index.js
+++ index.js
@@ -28,3 +28,21 @@ const questions = [
       "Please choose the color of your chosen shape. It can be a color keyword or a hexadecimal number.",
   },
 ];
+
+const run = async () => {
+  try {
+    const inquirerResult = await inquirer.prompt(questions);
+
+    console.log(inquirerResult);
+
+    const { text, textColor, shape, shapeColor } = inquirerResult;
+
+    if (text && textColor && shape && shapeColor) {
+      console.log(inquirerResult);
+      const createContent = await createSVG(inquirerResult);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+run();
